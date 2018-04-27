0f201e48021794b577a552de85bc16aa36da03ab
pac9 <pac9@btinternet.com>
2018-04-27 13:29:19 +0100

    initialisation of files

--- models/film.rb
+++ models/film.rb
@@ -2,6 +2,14 @@ require_relative('../db/sql_runner')
 
 class Film
 
+attr_reader :id
+attr_accessor :title, :price
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @price = options['price']
+  end
 
 
 
