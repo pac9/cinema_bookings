5c9b7e71e93936a2bc37ed78f50ceadc84f6be1a
pac9 <pac9@btinternet.com>
2018-04-27 13:28:14 +0100

    initialisation of files

--- db/cinema_bookings.sql
+++ db/cinema_bookings.sql
@@ -1,4 +1,4 @@
--- DROP TABLE tickets;
+DROP TABLE tickets;
 DROP TABLE films;
 DROP TABLE customers;
 
@@ -11,11 +11,11 @@ CREATE TABLE customers(
 CREATE TABLE films(
   id SERIAL4 PRIMARY KEY,
   title VARCHAR(255),
-  price VARCHAR(255)
+  price INT4
 );
 
--- CREATE TABLE tickets(
---   id SERIAL4 PRIMARY KEY,
---   customer_id INT4 REFERENCES customer(id) ON DELETE CASCADE,
---   film_id INT4 REFERENCES film(id) ON DELETE CASCADE
--- );
+CREATE TABLE tickets(
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+);
--- db/console.rb
+++ db/console.rb
@@ -4,12 +4,24 @@ require_relative('../models/ticket')
 
 require('pry')
 
+customer1 = Customer.new({"name" => "Jan", "funds" => 20})
+customer2 = Customer.new({"name" => "Celine", "funds" => 40})
+customer3 = Customer.new({"name" => "Norma", "funds" => 10})
+customer4 = Customer.new({"name" => "Kate", "funds" => 25})
+customer5 = Customer.new({"name" => "Penny", "funds" => 15})
 
+film1 = Film.new({"title" => "Jason Bourne", "price" => 5})
+film2 = Film.new({"title" => "Dirty Dancing", "price" => 3})
+film3 = Film.new({"title" => "Black Panther", "price" => 8})
 
-
-
-
-
+ticket1 = Ticket.new({"customer_id" => customer1.id, "film_id" => film1.id })
+ticket1 = Ticket.new({"customer_id" => customer1.id, "film_id" => film2.id })
+ticket1 = Ticket.new({"customer_id" => customer2.id, "film_id" => film3.id })
+ticket1 = Ticket.new({"customer_id" => customer3.id, "film_id" => film3.id })
+ticket1 = Ticket.new({"customer_id" => customer4.id, "film_id" => film1.id })
+ticket1 = Ticket.new({"customer_id" => customer4.id, "film_id" => film3.id })
+ticket1 = Ticket.new({"customer_id" => customer5.id, "film_id" => film1.id })
+ticket1 = Ticket.new({"customer_id" => customer5.id, "film_id" => film2.id })
 
 binding.pry
 nil
